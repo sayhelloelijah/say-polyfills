5d4ff32300994f296db2484a97c2b591d24e8eb5
sayhelloelijah <say@helloelijah.com>
2019-04-05 11:46:02 -0500

    add support for closest

---
+++ closest.js
@@ -0,0 +1,9 @@
+// closest polyfill
+this.Element && function(ElementPrototype) {
+    ElementPrototype.closest = ElementPrototype.closest ||
+        function(selector) {
+            var el = this;
+            while (el.matches && !el.matches(selector)) el = el.parentNode;
+            return el.matches ? el : null;
+        }
+}(Element.prototype);
